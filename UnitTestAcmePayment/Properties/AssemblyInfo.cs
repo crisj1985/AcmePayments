f540c6db2cb6d4b428b84c123461dfde600a5219
crisj1985 <crisj1985@hotmail.com>
2022-03-08 21:16:22 -0500

    Add UnitTest ok

---
+++ UnitTestAcmePayment/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestAcmePayment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestAcmePayment")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b3a6b25c-7a04-4d42-bf90-ad95f08f8c28")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestAcmePayment/UnitTest1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PayWork;
+
+namespace UnitTestAcmePayment
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ReadFileWorkEmp_FileExist()
+        {
+            string Path = @"C:\Users\CRISTIAN\source\repos\PayWork\WorkEmployee.txt";
+
+            List<EmployeeWork> expected = new List<EmployeeWork>();
+            List<DayWork> DwExpected = new List<DayWork>();
+            DwExpected.Add(new DayWork { Day = "MO", Hour = "10:00-12:00" });
+
+            expected.Add(new EmployeeWork { DaysWork = DwExpected, Name = "RENE" });
+            expected.Add(new EmployeeWork { DaysWork = DwExpected, Name = "ASTRID" });
+            expected.Add(new EmployeeWork { DaysWork = DwExpected, Name = "CRISTIAN" });
+
+            List<EmployeeWork> actual = Program.ReadFileWorkEmp(Path);
+
+            Assert.AreEqual(expected.Count, actual.Count);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void ReadFileWorkEmp_FileNotFound()
+        {
+            string Path = @"C:\Users\CRISTIAN\source\repos\PayWork\WorkEmploye.txt";
+
+            List<EmployeeWork> expected = new List<EmployeeWork>();
+            List<DayWork> DwExpected = new List<DayWork>();
+            DwExpected.Add(new DayWork { Day = "MO", Hour = "10:00-12:00" });
+
+            expected.Add(new EmployeeWork { DaysWork = DwExpected, Name = "RENE" });
+            expected.Add(new EmployeeWork { DaysWork = DwExpected, Name = "ASTRID" });
+            expected.Add(new EmployeeWork { DaysWork = DwExpected, Name = "CRISTIAN" });
+
+            List<EmployeeWork> actual = Program.ReadFileWorkEmp(Path);
+
+            Assert.AreEqual(expected.Count, actual.Count);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ReadFileWorkEmp_SeparatorNameDifferentInFile()
+        {
+            string Path = @"C:\Users\CRISTIAN\source\repos\PayWork\WorkEmployeeError.txt";
+
+            List<EmployeeWork> expected = new List<EmployeeWork>();
+            List<DayWork> DwExpected = new List<DayWork>();
+            DwExpected.Add(new DayWork { Day = "MO", Hour = "10:00-12:00" });
+
+            expected.Add(new EmployeeWork { DaysWork = DwExpected, Name = "RENE" });
+            expected.Add(new EmployeeWork { DaysWork = DwExpected, Name = "ASTRID" });
+            expected.Add(new EmployeeWork { DaysWork = DwExpected, Name = "CRISTIAN" });
+
+            List<EmployeeWork> actual = Program.ReadFileWorkEmp(Path);
+
+            Assert.AreEqual(expected.Count, actual.Count);
+        }
+        [TestMethod]
+        public void Amount_Successful()
+        {
+            DayWork objTest = new DayWork() { Day = "MO", Hour = "10:00-12:00" };
+            int a = Program.AmountBetweenWeek1;
+            int expected = 30;
+            int actual = Program.Amount(objTest);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(DayNoExistException))]
+        public void Amount_DayNoExist()
+        {
+            DayWork objTest = new DayWork() { Day = "MP", Hour = "10:00-12:00" };
+            int a = Program.AmountBetweenWeek1;
+            int expected = 30;
+            int actual = Program.Amount(objTest);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
